7e964383e1ac65cbdba3d253b19609c5bb6a013b
rociogsierra <113629813+rociogsierra@users.noreply.github.com>
2022-12-30 11:00:48 +0100

    he usado las variables x, y, z, para eliminar tres elementos del arbol

--- ejercicio8.py
+++ ejercicio8.py
@@ -3,10 +3,60 @@
 
 class Node:
     def __int__(self, data):
-        self.left = None
-        self.right = None
+        self.izq = None
+        self.der = None
         self.data = data
 
+#Eliminar 3 elementos del árbol
 
-
-
+#ELIMINO EL PRIMER ELEMENTO
+def eliminar_nodo(raiz, clave):
+    x = None
+    if (raiz is not None):
+        if (clave < raiz.data):
+            raiz.izq, x = eliminar_nodo(raiz.izq, clave)
+        elif (clave > raiz.data):
+            raiz.der, x = eliminar_nodo(raiz.der, clave)
+        else:
+            x = raiz.data
+            if(raiz.izq is None):
+                raiz = raiz.der
+            elif(raiz.der is None):
+                raiz = raiz.izq
+            else:
+                raiz.izq, aux = reemplazar(raiz.izq)
+                raiz.data = aux.data   
+#AHORA ELIMINO EL SEGUNDO ELEMENTO
+def eliminar_nodo(raiz, clave):
+    y = None
+    if (raiz is not None):
+        if (clave < raiz.data):
+            raiz.izq, y = eliminar_nodo(raiz.izq, clave)
+        elif (clave > raiz.data):
+            raiz.der, y = eliminar_nodo(raiz.der, clave)
+        else:
+            y = raiz.data
+            if(raiz.izq is None):
+                raiz = raiz.der
+            elif(raiz.der is None):
+                raiz = raiz.izq
+            else:
+                raiz.izq, aux = reemplazar(raiz.izq)
+                raiz.data = aux.data  
+#AHORA ELIMINO EL TERCER ELEMENTO
+def eliminar_nodo(raiz, clave):
+    z = None
+    if (raiz is not None):
+        if (clave < raiz.data):
+            raiz.izq, z = eliminar_nodo(raiz.izq, clave)
+        elif (clave > raiz.data):
+            raiz.der, z = eliminar_nodo(raiz.der, clave)
+        else:
+            z = raiz.data
+            if(raiz.izq is None):
+                raiz = raiz.der
+            elif(raiz.der is None):
+                raiz = raiz.izq
+            else:
+                raiz.izq, aux = reemplazar(raiz.izq)
+                raiz.data = aux.data  
