99073fec5a9ece376e4d9fc98b59af6a276ef445
rociogsierra <113629813+rociogsierra@users.noreply.github.com>
2022-12-30 10:41:59 +0100

    en un rango de 1000 numeros,, devolverlos aleatoriamente

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,2 +1,16 @@
 
-#
+#Desarrollar un algoritmo que permita cargar 1000 número enteros generados de manera aleatoria
+
+class Node:
+    def __int__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+import random
+
+for i in range(1000):
+    print(random.randint(0, 1000))
+
+    
+
--- operaciones.py
+++ operaciones.py
@@ -1,1 +1,5 @@
 
+print(f'{a} + {b} = {suma(a,b)}')
+print(f'{a} - {b} = {resta(a,b)}')
+print(f'{a} * {b} = {multiplicacion(a,b)}')
+print(f'{a} / {b} = {division(a,b)}')
