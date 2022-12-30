b891411a87b5ced877ca7759940140f83fc2dbc6
rociogsierra <113629813+rociogsierra@users.noreply.github.com>
2022-12-30 13:00:12 +0100

    en el cociente añado dos excepts (TypeError y ZeroDivisionError)

--- operaciones.py
+++ operaciones.py
@@ -1,13 +1,30 @@
 
+#SUMA
 def suma(primer_num, segundo_num):
     try:
         return primer_num + segundo_num
     except TypeError:
         print("ERROR: Tipo de dato no válido")
 
+#RESTA
 def resta(primer_num, segundo_num):
     try:
         return primer_num - segundo_num
     except TypeError:
         print("ERROR: Tipo de dato no válido")
     
+#MULTIPLICACIÓN
+def multiplicacion(primer_num, segundo_num):
+    try:
+        return primer_num * segundo_num
+    except TypeError:
+        print("ERROR: Tipo de dato no válido")
+
+#DIVISIÓN
+def division(primer_num, segundo_num):
+    try:
+        return primer_num / segundo_num
+    except TypeError:
+        print("ERROR: Tipo de dato no válido")
+    except ZeroDivisionError:
+        print("ERROR: No es posible dividir entre cero")
