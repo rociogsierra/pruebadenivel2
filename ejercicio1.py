e5a4176f6c5f1b0c3b71b675629bc908dacb26a6
rociogsierra <113629813+rociogsierra@users.noreply.github.com>
2023-01-02 20:39:56 +0100

    en suma, la funcion decisión consigue que este algoritmo devuelva las habilidades de cada superhéroe (que posteriormente son definidos en un diccionario) y luego imprime la misión que le será asignada a cada superhéroe

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,7 +8,6 @@ class NodoArbol(object):
 
 #Usaré la función insertar_nodo propuesta en el word de árboles y grafos
 #Si la respuesta es sí, se debe desplazar hacia el subárbol izquierdo, si es no al subárbol derecho
-
 def insertar_nodo(raiz, dato, pos):
     if(raiz is None):
         raiz = NodoArbol(dato)
