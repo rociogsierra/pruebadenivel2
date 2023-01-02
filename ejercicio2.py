04082126b6a7c73331160849d557445f9cd82727
rociogsierra <113629813+rociogsierra@users.noreply.github.com>
2023-01-02 17:27:35 +0100

    incluimos un bucle en caso de que la prioridad sea baja

--- ejercicio2.py
+++ ejercicio2.py
@@ -18,8 +18,33 @@ class pedidos_de_sugerencia():
         return "El Khan solicitante es {}, procedente del multiverso {}, cuya descripción es {} y su orden de prioridad {}".format(self.nombre_khan_solicitante, self.multiverso, self.descripcion, self.prioridad)
 
 #APARTADOS 3 y 4
-def orden_de_prioridad(orden):
+def orden_de_prioridad(prioridad):
     orden_nombre_khan_solicitante = {"Gran conquistador: mayor prioridad", "Khan que todo lo sabe: prioridad media"}
     orden_multiverso = {"Universo_616: mayor prioridad", "Universo_838: prioridad media"}
     orden_descripcion = {"El que permanece: mayor prioridad", "Carnicero de dioses: prioridad media"}
+
+    if (ordennombre == "mayor prioridad" or ordenmultiverso == "mayor prioridad" or ordendescripcion == "mayor prioridad"):
+        orden = "mayor prioridad"
+    elif (ordennombre == "prioridad media" or ordenmultiverso == "prioridad media" or ordendescripcion == "prioridad media"):
+        orden = "prioridad media"
+    else:
+        orden = "prioridad baja"
+    return orden        
+
+#APARTADO 5   
+#ahora definiremos la prioridad en función de los objetos en caso de que esta sea baja
+    try:
+        ordennombre = orden_nombre_khan_solicitante[prioridad.nombre_khan_solicitante]
+    except:
+        ordennombre = "prioridad baja"
+    try:    
+        ordenmultiverso = orden_multiverso[prioridad.multiverso]
+    except:
+        ordenmultiverso = "prioridad baja"
+    try:
+        ordendescripcion = orden_descripcion[prioridad.descripcion]
+    except:
+        ordendescripcion = "prioridad baja"
+
+
 
